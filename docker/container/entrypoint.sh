aab2d18b23145fb2524d9c0162631816aa8da4e9
Henrik Steen <henrist@henrist.net>
2016-08-01 17:09:56 +0200

    Add static ARP setup to docker setup

--- docker/container/entrypoint.sh
+++ docker/container/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# arp config is done to avoid arp lookups that causes loss
+
 setup_client() {
     local iface=$(ip route show to 10.25.1.0/24 | awk '{print $3}')
     echo "Adding route to servers through aqm-machine"
@@ -7,6 +9,7 @@ setup_client() {
     (set -x && ip route add 10.25.3.0/24 via 10.25.1.2 dev $iface)
     (set -x && tc qdisc add dev $iface root handle 1: pfifo_fast)
     (set -x && ip link set $iface txqueuelen 1000)
+    (set -x && arp -i $iface -s 10.25.1.2 02:42:0a:19:01:02)
 
     echo "export IFACE_AQM=$iface" >/tmp/testbed-vars-local.sh
 }
@@ -18,6 +21,9 @@ setup_server() {
     (set -x && ip route add 10.25.1.0/24 via ${1}.2 dev $iface)
     (set -x && tc qdisc add dev $iface root handle 1: pfifo_fast)
     (set -x && ip link set $iface txqueuelen 1000)
+
+    (set -x && arp -i $iface -s ${1}.2 02:42:0a:19:0${1/*.}:02)
+
     echo "export IFACE_AQM=$iface" >/tmp/testbed-vars-local.sh
 
     #echo "Adding route to other servers through aqm-machine"
@@ -38,16 +44,19 @@ setup_aqm() {
     echo "export IFACE_CLIENTS=$iface" >>/tmp/testbed-vars-local.sh
     (set -x && tc qdisc add dev $iface root handle 1: pfifo_fast)
     (set -x && ip link set $iface txqueuelen 1000)
+    (set -x && arp -i $iface -s 10.25.1.11 02:42:0a:19:01:0b)
 
     local iface=$(ip route show to 10.25.2.0/24 | awk '{print $3}')
     echo "export IFACE_SERVERA=$iface" >>/tmp/testbed-vars-local.sh
     (set -x && tc qdisc add dev $iface root handle 1: pfifo_fast)
     (set -x && ip link set $iface txqueuelen 1000)
+    (set -x && arp -i $iface -s 10.25.2.21 02:42:0a:19:02:15)
 
     local iface=$(ip route show to 10.25.3.0/24 | awk '{print $3}')
     echo "export IFACE_SERVERB=$iface" >>/tmp/testbed-vars-local.sh
     (set -x && tc qdisc add dev $iface root handle 1: pfifo_fast)
     (set -x && ip link set $iface txqueuelen 1000)
+    (set -x && arp -i $iface -s 10.25.2.31 02:42:0a:19:03:1f)
 
     names=(CLIENTA CLIENTB SERVERA SERVERB)
     nets=(10.25.1.0/24 10.25.1.0/24 10.25.2.0/24 10.25.3.0/24)
