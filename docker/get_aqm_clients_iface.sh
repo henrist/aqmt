97dfdd72530956b9fd843122192dcaf22519e21b
Henrik Steen <henrist@henrist.net>
2016-10-10 09:51:56 +0200

    Docker: List interfaces for all containers

--- docker/get_aqm_clients_iface.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-ifnum=$(docker-compose exec aqm ip a show to 10.25.1.0/24 \
-        | head -1 | awk -F: '{print $1}')
-
-iface=$(ip l | grep "@if$ifnum" | sed 's/.*: \(veth.*\)@.*/\1/')
-
-echo "$iface"
---
+++ docker/list_ifaces.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+show_iface() {
+  name=$1
+  cidr=$2
+  comment=$3
+
+  ifnum=$(docker-compose exec $name ip a show to $cidr \
+          | head -1 | awk -F: '{print $1}')
+
+  iface=$(ip l | grep "@if$ifnum" | sed 's/.*: \(veth.*\)@.*/\1/')
+
+  echo "$name: $iface ($comment)"
+}
+
+show_iface aqm 10.25.1.0/24 "to clients"
+show_iface aqm 10.25.2.0/24 "to server a"
+show_iface aqm 10.25.3.0/24 "to server b"
+echo
+show_iface clienta 10.25.1.0/24 "to aqm"
+show_iface clientb 10.25.1.0/24 "to aqm"
+echo
+show_iface servera 10.25.2.0/24 "to aqm"
+show_iface serverb 10.25.3.0/24 "to aqm"
