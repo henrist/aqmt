e3a3731ac5cf5d2a534542b521b2b77820104ec3
Henrik Steen <henrist@henrist.net>
2016-10-25 03:19:37 +0200

    Plot hierarchical data sets

--- henrste/tests/plot.py
+++ henrste/tests/plot.py
@@ -10,13 +10,223 @@ color_ecn_cubic = "black"
 color_reno = "brown"
 color_util = "orange"
 
+class HierarchyPlot():
+    """Plot a collection of test cases
+
+    It is assumed that all leafs are of the same structure,
+    but not necessary same amount of test cases inside
+    a leaf
+    """
+
+    def __init__(self):
+        self.plotutils = Plot()
+
+    def get_num_testcases(self, testmeta):
+        """Returns a tuple of numbers sets, tests and depth"""
+
+        sets = 0
+        tests = 0
+        depth = 0
+
+        def traverse(testmeta, depthnow=0):
+            nonlocal sets, tests, depth
+            f = testmeta['children'][0]
+
+            if depthnow > depth:
+                depth = depthnow
+
+            # is this a set of tests?
+            if 'testcase' in f:
+                tests += len(testmeta['children'])
+                sets += 1
+
+            # or is it a list of sets
+            else:
+                for item in testmeta['children']:
+                    traverse(item, depthnow + 1)
+
+        traverse(testmeta)
+        return (sets, tests, depth)
+
+    def walk_tree_leaf_set(self, testmeta, fn):
+        """Walks the tree and calls fn for every leaf set"""
+
+        first_set = True
+        x = 0
+
+        def walk(testmeta):
+            nonlocal first_set, x
+
+            if len(testmeta['children']) == 0:
+                return
+
+            f = testmeta['children'][0]
+
+            # is this a set of tests?
+            if 'testcase' in f:
+                fn(testmeta, first_set, x)
+                first_set = False
+                x += len(testmeta['children'])
+
+            # or is it a list of sets
+            else:
+                for item in testmeta['children']:
+                    walk(item)
+
+            x += 1
+
+        walk(testmeta)
+
+    def walk_tree_set(self, testmeta, fn):
+        """Walks the tree and calls fn for every set in reverse order"""
+        x = 0
+
+        def walk(testmeta, depth):
+            nonlocal x
+
+            if len(testmeta['children']) == 0:
+                return
+
+            f = testmeta['children'][0]
+
+            # is this a set of tests?
+            if 'testcase' in f:
+                x += len(testmeta['children'])
+
+            # or is it a list of sets
+            else:
+                for item in testmeta['children']:
+                    y = x
+                    walk(item, depth + 1)
+                    fn(item, y, depth, x - y)
+
+            x += 1
+
+        walk(testmeta, 0)
+
+    def plot(self, outfile, testmeta):
+
+        n_sets, n_tests, n_depth = self.get_num_testcases(testmeta)
+        num = n_sets + n_tests
+
+        self.plotutils.size = '21cm,22cm'
+        self.plotutils.header()
+
+        def plot_labels(testmeta, x, depth, width):
+            #print(testmeta['title'], depth, x, width)
+            self.plotutils.gpi += """
+                set label '""" + testmeta['title'] + """' at first """ + str(x+width/2) + """, graph """ + str(1.05 + 0.07 * (n_depth - depth - 1)) + """ font 'Times-Roman,10pt' tc rgb 'black' center"""
+
+        self.plotutils.gpi += """
+            set multiplot layout 3,1 title '""" + testmeta['title'] + """'
+
+            unset bars
+            set xtic rotate by -65 font ',10pt'
+            set key right center
+
+            #set title '""" + testmeta['title'] + """'
+            set xrange [-.6:""" + str(num-2) + """.9]
+            set yrange [0:]
+            set boxwidth 0.2
+            set tmargin """ + str(1 * n_depth + 1) + """
+            set lmargin 13"""
+
+
+        self.plotutils.gpi += """
+
+            #set style line 100 lt 1 lc rgb 'red' lw 2
+            #set arrow 100 from 1,-1 to 1,1 nohead ls 100 front
+            #set arrow 100 from graph 0, second 100 to graph 1, second 100 nohead ls 100 front
+
+            #set xtic offset first .3
+            set ylabel "Percent\\n{/Times:Italic=10 (p_1, mean, p_{99})}" """
+
+        self.walk_tree_set(testmeta, plot_labels)
+
+        plot = ''
+        def data_util(testmeta, is_first_set, x):
+            nonlocal plot
+
+            self.plotutils.gpi += """
+                $dataUtil""" + str(x) + """ << EOD
+                """ + self.plotutils.mergeTestcaseData(testmeta['children'], 'util_stats') + """
+                EOD"""
+
+            plot += "$dataUtil" + str(x) + "  using ($0+" + str(x) + "+0.0):3:5:4       with yerrorbars ls 1 pointtype 7 pointsize 0.5 lw 1.5 title '" + ('Total utilization' if is_first_set else '') + "', "
+            plot += "                      '' using ($0+" + str(x) + "+0.0):3  with lines lc rgb 'gray'         title '', "
+            plot += "                      '' using ($0+" + str(x) + "+0.1):6:8:7:xtic(1)    with yerrorbars ls 2 pointtype 7 pointsize 0.5 lw 1.5 title '" + ('ECN utilization' if is_first_set else '') + "', "
+            plot += "                      '' using ($0+" + str(x) + "+0.1):6  with lines lc rgb 'gray'         title '', "
+            plot += "                      '' using ($0+" + str(x) + "+0.2):9:10:11  with yerrorbars ls 3 pointtype 7 pointsize 0.5 lw 1.5 title '" + ('Non-ECN utilization' if is_first_set else '') + "', "
+            plot += "                      '' using ($0+" + str(x) + "+0.2):9  with lines lc rgb 'gray'         title '', "
+
+        self.walk_tree_leaf_set(testmeta, data_util)
+        self.plotutils.gpi += """
+            plot """ + plot + """
+            set ylabel "Queueing delay [ms]\\n{/Times:Italic=10 (p_1, mean, p_{99})}"
+            set xtic offset first .15"""
+
+        plot = ''
+        def data_rate(testmeta, is_first_set, x):
+            nonlocal plot
+
+            self.plotutils.gpi += """
+                $data_qs_ecn_stats""" + str(x) + """ << EOD
+                """ + self.plotutils.mergeTestcaseData(testmeta['children'], 'qs_ecn_stats') + """
+                EOD
+                $data_qs_nonecn_stats""" + str(x) + """ << EOD
+                """ + self.plotutils.mergeTestcaseData(testmeta['children'], 'qs_nonecn_stats') + """
+                EOD"""
+
+            plot += "$data_qs_ecn_stats" + str(x) + "    using ($0+" + str(x) + "+0.05):3:5:4:xtic(1)   with yerrorbars ls 3 lw 1.5 pointtype 7 pointsize 0.5            title '" + ('ECN queue' if is_first_set else '') + "', "
+            plot += "                              ''    using ($0+" + str(x) + "+0.05):3  with lines lc rgb 'gray'         title '', "
+            plot += "$data_qs_nonecn_stats" + str(x) + " using ($0+" + str(x) + "+0.15):3:5:4  with yerrorbars ls 5 lw 1.5 pointtype 7 pointsize 0.5           title '" + ('Non-ECN queue' if is_first_set else '') + "', "
+            plot += "                              ''    using ($0+" + str(x) + "+0.15):3  with lines lc rgb 'gray'         title '', "
+
+        self.walk_tree_leaf_set(testmeta, data_rate)
+        self.plotutils.gpi += """
+            plot """ + plot + """
+            #set xtic offset first .10
+            set ylabel "Percent\\n{/Times:Italic=10 (p_1, mean, p_{99})}"
+            set xlabel '""" + testmeta['xlabel'] + """'"""
+
+        plot = ''
+        def data_drops(testmeta, is_first_set, x):
+            nonlocal plot
+
+            self.plotutils.gpi += """
+                $data_d_percent_nonecn_stats""" + str(x) + """ << EOD
+                """ + self.plotutils.mergeTestcaseData(testmeta['children'], 'd_percent_nonecn_stats') + """
+                EOD
+                $data_d_percent_ecn_stats""" + str(x) + """ << EOD
+                """ + self.plotutils.mergeTestcaseData(testmeta['children'], 'd_percent_ecn_stats') + """
+                EOD
+                $data_m_percent_ecn_stats""" + str(x) + """ << EOD
+                """ + self.plotutils.mergeTestcaseData(testmeta['children'], 'm_percent_ecn_stats') + """
+                EOD"""
+
+            plot += "$data_d_percent_nonecn_stats" + str(x) + "  using ($0+" + str(x) + "+0.0):3:5:4 with yerrorbars ls 3 pointtype 7 pointsize 0.5 lw 1.5  title '" + ('Drops (Non-ECN)' if is_first_set else '') + "', "
+            plot += "                                         '' using ($0+" + str(x) + "+0.0):3     with lines lc rgb 'gray'         title '', "
+            plot += "$data_d_percent_ecn_stats" + str(x) + "     using ($0+" + str(x) + "+0.10):3:5:4:xtic(1) with yerrorbars lc rgb 'red' pointtype 7 pointsize 0.5 lw 1.5  title '" + ('Drops (ECN)' if is_first_set else '') + "', "
+            plot += "                                         '' using ($0+" + str(x) + "+0.10):3     with lines lc rgb 'gray'         title '', "
+            plot += "$data_m_percent_ecn_stats" + str(x) + "     using ($0+" + str(x) + "+0.20):3:5:4 with yerrorbars ls 8 pointtype 7 pointsize 0.5 lw 1.5  title '" + ('Marks (ECN)' if is_first_set else '') + "', "
+            plot += "                                         '' using ($0+" + str(x) + "+0.20):3     with lines lc rgb 'gray'         title '', "
+
+        self.walk_tree_leaf_set(testmeta, data_drops)
+        self.plotutils.gpi += """
+            plot """ + plot + """
+            unset multiplot"""
+
+        self.plotutils.generate(outfile)
+
+
+
 class Plot():
     gpi = ''
     size = '21cm,10cm'
 
     def generate(self, outputFile):
         self.gpi = """
-            #reset
+            reset
             set terminal pdfcairo font 'Times-Roman,12' size """ + self.size + """
             set output '""" + outputFile + """.pdf'""" + self.gpi
 
@@ -25,7 +235,7 @@ class Plot():
         with open(outputFile + '.gpi', 'w') as f:
             f.write(self.gpi)
 
-        local['gnuplot'][outputFile + '.gpi']()
+        local['gnuplot'][outputFile + '.gpi'].run(stdin=None, stdout=None, stderr=None, retcode=None)
 
         self.gpi = ''
 
@@ -42,7 +252,38 @@ class Plot():
             #set boxwidth 0.4
 
             #set label 2 'RTT[ms]:' at screen 0.25,0.6 front font 'Times-Roman,120' tc rgb 'black' left
-            #set label 30 'PI2' at screen 1,3.4 font 'Times-Roman,140' tc rgb 'black' left"""
+            #set label 30 'PI2' at screen 1,3.4 font 'Times-Roman,140' tc rgb 'black' left
+
+
+            # from https://github.com/aschn/gnuplot-colorbrewer
+            # line styles for ColorBrewer Dark2
+            # for use with qualitative/categorical data
+            # provides 8 dark colors based on Set2
+            # compatible with gnuplot >=4.2
+            # author: Anna Schneider
+
+            # line styles
+            set style line 1 lc rgb '#1B9E77' # dark teal
+            set style line 2 lc rgb '#D95F02' # dark orange
+            set style line 3 lc rgb '#7570B3' # dark lilac
+            set style line 4 lc rgb '#E7298A' # dark magenta
+            set style line 5 lc rgb '#66A61E' # dark lime green
+            set style line 6 lc rgb '#E6AB02' # dark banana
+            set style line 7 lc rgb '#A6761D' # dark tan
+            set style line 8 lc rgb '#666666' # dark gray
+
+            # palette
+            set palette maxcolors 8
+            set palette defined ( 0 '#1B9E77',\
+                                  1 '#D95F02',\
+                                  2 '#7570B3',\
+                                  3 '#E7298A',\
+                                  4 '#66A61E',\
+                                  5 '#E6AB02',\
+                                  6 '#A6761D',\
+                                  7 '#666666' )
+
+            """
 
 
     def plot_compare_flows(self, folder, testfolders):
@@ -51,12 +292,12 @@ class Plot():
         num = int((local['wc']['-l', folder + '/util_stats'] | local['awk']['{print $1-1}'])())
 
         self.size = '21cm,22cm'
-        self.header(num=num)
+        self.header()
 
         self.gpi += """
             set multiplot layout 3,1
 
-            set title '""" + folder + """'
+            set title 'test""" + folder + """'
             set xrange [-.6:""" + str(num-1) + """.9]
             set yrange [0:]
             set boxwidth 0.2
@@ -114,6 +355,7 @@ class Plot():
 
 
     def plot_multiple_flows(self, testfolders, output_path):
+        """Generate a PDF with one page with graphs per flow"""
         #self.gpi += """
         #    set multiplot layout 4,""" + str(len(testfolders)) + """ columnsfirst"""
 
@@ -176,7 +418,7 @@ class Plot():
         self.gpi += """
             """
 
-        self.header(2)
+        self.header()
 
         self.gpi += """
 
@@ -209,6 +451,40 @@ class Plot():
             #    unset multiplot"""
 
             self.generate(testfolder + '/analysis')
+
+    def getTag(self, testcase):
+        with open(testcase['testcase'] + '/details', 'r') as f:
+            for line in f:
+                if line.startswith('x_udp_rate'):
+                    return str(int(int(line.split()[1]) / 1000))
+
+        return 'n/a'
+
+    def mergeTestcaseData(self, testcases, statsname):
+        out = []
+        for testcase in testcases:
+            with open(testcase['testcase'] + '/' + statsname, 'r') as f:
+                tag = self.getTag(testcase)
+
+                for line in f:
+                    if line.startswith('#'):
+                        continue
+
+                    out.append(tag + ' ' + line)
+
+        return ''.join(out)
+
+
+#class TestCase():
+#    def __init__(self, testfolder):
+#        self.testfolder = testfolder
+
+#class TestSet():
+#    children = []
+
+#    def addTest(self, testcase):
+#        if len(children)
+
 
 if __name__ == '__main__':
     plot = Plot()
@@ -244,3 +520,85 @@ if __name__ == '__main__':
             'testset-speeds/nonect/test-015',
             'testset-speeds/nonect/test-016',
             'testset-speeds/nonect/test-017'])
+
+    if True:
+        hp = HierarchyPlot()
+        hp.plot('testset-speeds/nonect/testgraph', {
+            'title': 'UDP with Non-ECT',
+            'xlabel': 'UDP bitrate [kbps]',
+            'children': [
+                {'testcase': 'testset-speeds/nonect/test-001'},
+                {'testcase': 'testset-speeds/nonect/test-002'},
+                {'testcase': 'testset-speeds/nonect/test-003'},
+                {'testcase': 'testset-speeds/nonect/test-004'},
+                {'testcase': 'testset-speeds/nonect/test-005'},
+                {'testcase': 'testset-speeds/nonect/test-006'},
+                {'testcase': 'testset-speeds/nonect/test-007'},
+                {'testcase': 'testset-speeds/nonect/test-008'},
+                {'testcase': 'testset-speeds/nonect/test-009'},
+                {'testcase': 'testset-speeds/nonect/test-010'},
+                {'testcase': 'testset-speeds/nonect/test-011'},
+                {'testcase': 'testset-speeds/nonect/test-012'},
+                {'testcase': 'testset-speeds/nonect/test-013'},
+                {'testcase': 'testset-speeds/nonect/test-014'},
+                {'testcase': 'testset-speeds/nonect/test-015'},
+                {'testcase': 'testset-speeds/nonect/test-016'},
+                {'testcase': 'testset-speeds/nonect/test-017'},
+            ]
+        })
+
+    if False:
+        hp = HierarchyPlot()
+        hp.plot('testset-speeds/analysis', {
+            'title': 'Speed testing',
+            'xlabel': 'UDP bitrate [kbps]',
+            'children': [{
+                'title': 'UDP with Non-ECT',
+                'children': [
+                    {'testcase': 'testset-speeds/nonect/test-001'},
+                    {'testcase': 'testset-speeds/nonect/test-002'},
+                    {'testcase': 'testset-speeds/nonect/test-003'},
+                    {'testcase': 'testset-speeds/nonect/test-004'},
+                    {'testcase': 'testset-speeds/nonect/test-005'},
+                    {'testcase': 'testset-speeds/nonect/test-006'},
+                    {'testcase': 'testset-speeds/nonect/test-007'},
+                    {'testcase': 'testset-speeds/nonect/test-008'},
+                    {'testcase': 'testset-speeds/nonect/test-009'},
+                    {'testcase': 'testset-speeds/nonect/test-010'},
+                    {'testcase': 'testset-speeds/nonect/test-011'},
+                    {'testcase': 'testset-speeds/nonect/test-012'},
+                    {'testcase': 'testset-speeds/nonect/test-013'},
+                    {'testcase': 'testset-speeds/nonect/test-014'},
+                    {'testcase': 'testset-speeds/nonect/test-015'},
+                    {'testcase': 'testset-speeds/nonect/test-016'},
+                    {'testcase': 'testset-speeds/nonect/test-017'},
+                ]
+            }, {
+                'title': 'UDP with ECT(1)',
+                'children': [
+                    #{
+                    #    'title': 'tester a',
+                    #    'children': [
+                    {'testcase': 'testset-speeds/ect1/test-001'},
+                    {'testcase': 'testset-speeds/ect1/test-002'},
+                    {'testcase': 'testset-speeds/ect1/test-003'},
+                    {'testcase': 'testset-speeds/ect1/test-004'},
+                    {'testcase': 'testset-speeds/ect1/test-005'},
+                    {'testcase': 'testset-speeds/ect1/test-006'},
+                    {'testcase': 'testset-speeds/ect1/test-007'},
+                    {'testcase': 'testset-speeds/ect1/test-008'},
+                    {'testcase': 'testset-speeds/ect1/test-009'},
+                    {'testcase': 'testset-speeds/ect1/test-010'},
+                    {'testcase': 'testset-speeds/ect1/test-011'},
+                    {'testcase': 'testset-speeds/ect1/test-012'},
+                    #]},{
+                    #    'title': 'Tester noe mer',
+                    #    'children': [
+                    {'testcase': 'testset-speeds/ect1/test-013'},
+                    {'testcase': 'testset-speeds/ect1/test-014'},
+                    {'testcase': 'testset-speeds/ect1/test-015'},
+                    {'testcase': 'testset-speeds/ect1/test-016'},
+                    {'testcase': 'testset-speeds/ect1/test-017'},
+                    #]}
+                ]
+            }]})
