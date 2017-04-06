8f72ede10f1a100103c3d7861618d870f7f5f1c3
Henrik Steen <henrist@henrist.net>
2017-04-06 21:58:49 +0200

    Also build ta and greedy when loading dependencies

---
+++ load_dependencies.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+cd "$(dirname $(readlink -f $BASH_SOURCE))"
+
+(
+    echo
+    echo "---- BUILDING: traffic_analyzer ----"
+    echo
+    cd traffic_analyzer
+    make
+)
+
+(
+    echo
+    echo "---- BUILDING: greedy_generator ----"
+    echo
+    cd greedy_generator
+    make
+)
+
+for n in sch_fifo_qsize sch_pi2 sch_pie_qsize tcp_dctcp; do (
+    echo
+    echo "---- BUILDING AND LOADING: $n ----"
+    echo
+    cd $n
+    make clean
+    make
+    sudo make unload >/dev/null 2>&1
+    sudo make load
+); done
