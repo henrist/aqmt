a7d58003af4ba55e30aec78dd76370ce6fb3cb57
Henrik Steen <henrist@henrist.net>
2017-04-13 00:23:08 +0200

    analyzer: Remove more demo stuff I missed earlier

--- henrste/framework/ta/analyzer.cpp
+++ henrste/framework/ta/analyzer.cpp
@@ -24,8 +24,6 @@
 #include <time.h>
 #include <sys/types.h>
 
-#define DEMO_QLIM 140
-#define DEMO_RTT 0.007
 #define NSEC_PER_SEC 1000000000UL
 #define NSEC_PER_MS 1000000UL
 #define US_PER_S 1000000UL
@@ -52,55 +50,6 @@ uint64_t getStamp()
     struct timespec monotime;
     clock_gettime(CLOCK_MONOTONIC, &monotime);
     return ((uint64_t)monotime.tv_sec) * US_PER_S + monotime.tv_nsec / NSEC_PER_US;
-}
-
-DemoData::DemoData()
-{
-    ipclass = 0;
-    pthread_mutexattr_t errorcheck;
-    pthread_mutexattr_init(&errorcheck);
-    pthread_mutex_init(&mutex, &errorcheck);
-    pthread_cond_init(&newdata, NULL);
-    init();
-    linkcap = 5000000;
-    rtt_ecn = 0;
-    rtt_nonecn = 0;
-    rtt_base = 0.007;
-}
-
-void DemoData::init()
-{
-    util = 0;
-    mark_ecn = 0;
-    drop_ecn = 0;
-    mark_nonecn = 0;
-    drop_nonecn = 0;
-    fair_rate = 0;
-    fair_window = 0;
-    alrate_ecn = 0;
-    alrate_nonecn = 0;
-    alw_ecn = 0;
-    alw_nonecn = 0;
-    cbrrate_ecn = 0;
-    cbrrate_nonecn = 0;
-
-    ecn_th.clear();
-    nonecn_th.clear();
-    ecn_w.clear();
-    nonecn_w.clear();
-
-    ecn_th.resize(10);
-    nonecn_th.resize(10);
-    ecn_w.resize(10);
-    nonecn_w.resize(10);
-    avg_qsize_ll = 0;
-    avg_qsize_c = 0;
-    p99_qsize_ll = 0;
-    p99_qsize_c = 0;
-    ll_qsize_y.clear();
-    c_qsize_y.clear();
-    ll_qsize_y.resize(DEMO_QLIM);
-    c_qsize_y.resize(DEMO_QLIM);
 }
 
 ThreadParam::ThreadParam(pcap_t *descr, uint32_t sinterval, char *folder, uint32_t nrs, bool ipc)
--- henrste/framework/ta/analyzer.h
+++ henrste/framework/ta/analyzer.h
@@ -122,44 +122,6 @@ public:
     }
 };
 
-struct DemoData {
-public:
-    bool ipclass;
-    std::vector<double> ecn_th;
-    std::vector<double> nonecn_th;
-    std::vector<double> ecn_w;
-    std::vector<double> nonecn_w;
-
-    long long util;
-    double rtt_base;
-    double rtt_ecn;
-    double rtt_nonecn;
-    double linkcap;
-    double mark_ecn;
-    double drop_ecn;
-    double mark_nonecn;
-    double drop_nonecn;
-    double fair_rate;
-    double fair_window;
-    double alrate_ecn;
-    double alrate_nonecn;
-    double alw_ecn;
-    double alw_nonecn;
-    double cbrrate_ecn;
-    double cbrrate_nonecn;
-
-    std::vector<double> ll_qsize_y;
-    std::vector<double> c_qsize_y;
-    double avg_qsize_ll;
-    double avg_qsize_c;
-    double p99_qsize_ll;
-    double p99_qsize_c;
-    pthread_mutex_t mutex;
-    pthread_cond_t newdata;
-    DemoData();
-    void init();
-};
-
 struct ThreadParam {
 public:
     uint64_t packets_captured;
