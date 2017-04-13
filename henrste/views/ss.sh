678d678759f207f7e3bf04f5521c8ed84b6e297a
Henrik Steen <henrist@henrist.net>
2017-04-13 17:48:46 +0200

    utils/ss: Improve filter for traffic

--- henrste/views/ss.sh
+++ henrste/views/ss.sh
@@ -4,8 +4,10 @@
 check() {
     title=$1
     host=$2
+    remote1=$3
+    remote2=$4
     ret=$(ssh $host "
-        ss -nei --memory '( ( sport >= :5500 and sport <= :5600 ) or ( dport >= :5500 and dport <= :5600 ) )' | tail -n+2
+        ss -nei --memory '( src $remote1 or dst $remote1 or src $remote2 or dst $remote2 )' | tail -n+2
         ")
 
     if [ -n "$ret" ]; then
@@ -26,7 +28,9 @@ check() {
     fi
 }
 
-check clienta $IP_CLIENTA_MGMT
-check servera $IP_SERVERA_MGMT
-check clientb $IP_CLIENTB_MGMT
-check serverb $IP_SERVERB_MGMT
+# a client can talk with two servers
+# a server can talk with two clients
+check clienta $IP_CLIENTA_MGMT $IP_SERVERA $IP_SERVERB
+check servera $IP_SERVERA_MGMT $IP_CLIENTA $IP_CLIENTB
+check clientb $IP_CLIENTB_MGMT $IP_SERVERA $IP_SERVERB
+check serverb $IP_SERVERB_MGMT $IP_CLIENTA $IP_CLIENTB
