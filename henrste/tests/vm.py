fe3bb21188fe36464b9808ae77beeea6134af24f
Henrik Steen <henrist@henrist.net>
2017-04-15 04:04:57 +0200

    tests(vm): Update test

--- henrste/tests/vm.py
+++ henrste/tests/vm.py
@@ -6,6 +6,7 @@ import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 
 from framework import MBIT, Testbed, TestEnv, run_test, steps
+from framework.plot import PlotAxis, collection_components
 from framework.traffic import greedy, udp
 import time
 from utils import hostname
@@ -16,7 +17,7 @@ def test(result_folder):
     def my_test(testcase):
         testdef = testcase.testenv.testdef
 
-        for i in range(15):
+        for i in range(10):
             testcase.traffic(greedy, node='a', tag='node-a')
             testcase.traffic(greedy, node='b', tag='node-b')
 
@@ -26,9 +27,9 @@ def test(result_folder):
 
     testbed = Testbed()
 
-    testbed.ta_samples = 300
-    testbed.ta_idle = 5
-    testbed.ta_delay = 250
+    testbed.ta_samples = 500
+    #testbed.ta_idle = 5
+    testbed.ta_delay = 125
 
     testbed.cc('a', 'cubic', testbed.ECN_ALLOW)
     testbed.cc('b', 'dctcp-drop', testbed.ECN_INITIATE)
@@ -36,10 +37,20 @@ def test(result_folder):
     run_test(
         folder=result_folder,
         title='Testing VM',
-        subtitle='Using 15 flows of CUBIC, 15 flows of DCTCP (with ECN) and 1 flow UDP',
+        subtitle='Using 10 flows of CUBIC, 10 flows of DCTCP (with ECN) and 1 flow UDP',
         testenv=TestEnv(testbed, retest=False),
         steps=(
-            steps.plot_compare(),
+            steps.plot_compare(x_axis=PlotAxis.LINEAR, swap_levels=[3,2,1,0], components=[
+                collection_components.utilization_queues(),
+                collection_components.utilization_tags(),
+                collection_components.window_rate_ratio(y_logarithmic=True),
+                collection_components.window_rate_ratio(),
+                collection_components.queueing_delay(y_logarithmic=True),
+                collection_components.queueing_delay(),
+                collection_components.drops_marks(y_logarithmic=True),
+                collection_components.drops_marks(),
+            ]),
+            steps.plot_flows(swap_levels=[3,2,1,0]),
             steps.branch_runif([
                 ('simula', lambda testenv: hostname() == 'ford', 'Simula testbed'),
                 ('simula-desktop', lambda testenv: hostname() == 'DARASK-SM', 'Simula docker'),
@@ -49,24 +60,24 @@ def test(result_folder):
             steps.branch_sched([
                 # tag, title, name, params
                 ('pi2',
-                    'PI2: dc_dualq dc_ecn target 15ms tupdate 15ms alpha 5 beta 50 k 2 t\\\\_shift 30ms l\\\\_drop 100',
+                    'PI2',
                     'pi2', 'dc_dualq dc_ecn target 15ms tupdate 15ms alpha 5 beta 50 k 2 t_shift 30ms l_drop 100'),
                 ('pie', 'PIE', 'pie', 'ecn target 15ms tupdate 15ms alpha 1 beta 10 ecndrop 25'),
-                #('pfifo', 'pfifo', 'pfifo', ''),
+                ('pfifo', 'pfifo', 'pfifo', ''),
             ]),
             steps.branch_rtt([
                 10,
                 100,
-            ]),
+            ], title='%d ms'),
             steps.branch_bitrate([
                 100,
                 250,
                 500,
-            ]),
+            ], title='%d', titlelabel='Linkrate [Mb/s]'),
             steps.branch_define_udp_rate([
                 50,
                 300,
-            ]),
+            ], title='%g'),
             steps.branch_repeat(8),
             my_test,
         ),
