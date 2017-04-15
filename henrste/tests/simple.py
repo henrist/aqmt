a6febfb9e5dcfe33d2f4ecd08d0dbaff9a833eb4
Henrik Steen <henrist@henrist.net>
2017-04-15 04:05:29 +0200

    tests(simple): Update test

--- henrste/tests/simple.py
+++ henrste/tests/simple.py
@@ -14,13 +14,13 @@ def test():
 
     def my_test(testcase):
         testcase.traffic(greedy, node='a', tag='cubic 1')
-        testcase.traffic(greedy, node='a', tag='cubic 2')
+        #testcase.traffic(greedy, node='a', tag='cubic 2')
         testcase.traffic(greedy, node='b', tag='ecn-cubic 1')
-        testcase.traffic(greedy, node='b', tag='ecn-cubic 2')
+        #testcase.traffic(greedy, node='b', tag='ecn-cubic 2')
 
     testbed = Testbed()
 
-    testbed.ta_samples = 200
+    testbed.ta_samples = 300
     testbed.ta_idle = 0
     testbed.ta_delay = 125
 
@@ -31,10 +31,11 @@ def test():
     run_test(
         folder='results/simple',
         title='Just a simple test to verify setup',
-        testenv=TestEnv(testbed, retest=True, reanalyze=True),
+        testenv=TestEnv(testbed, retest=False, reanalyze=True),
         steps=(
             steps.plot_compare(swap_levels=[1], components=[
                 collection_components.utilization_tags(),
+                collection_components.window_rate_ratio(),
                 collection_components.queueing_delay(),
                 collection_components.drops_marks(),
             ]),
@@ -51,7 +52,7 @@ def test():
                 2,
                 10,
                 50,
-                100,
+                #100,
             ], title='%d'),
             steps.branch_bitrate([
                 10,
