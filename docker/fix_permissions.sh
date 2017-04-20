193cb044572d6464ff83a9b735bb14a93a0acaa8
Henrik Steen <henrist@henrist.net>
2017-04-21 00:22:57 +0200

    Add a container to watch and fix file permissions

--- docker/fix_permissions.sh
+++ docker/fix_permissions.sh
@@ -1,9 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ $(id -u) != 0 ]; then
     echo "Run as root!"
     exit 1
 fi
+
+cd "$(dirname $(readlink -f $BASH_SOURCE))"
 
 d="../henrste"
 
@@ -11,5 +13,5 @@ chown -R $(stat -c '%u' "$d"):$(stat -c '%g' "$d") "$d"
 
 inotifywait -m -r "$d" -e create --format '%w%f' | while read f; do
     chown $(stat -c '%u' "$d"):$(stat -c '%g' "$d") "$f"; \
-    echo "Fixed $f"; \
+    echo "$(date +%H:%M:%S.%N) Fixed $f"; \
 done
