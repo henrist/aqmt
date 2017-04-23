101cedb1bb7023d679a8960b3d6e2023ea4e6f76
Henrik Steen <henrist@henrist.net>
2017-04-23 02:20:33 +0200

    docker: Ignore .git folders for fix-permissions

--- docker/fix-permissions.sh
+++ docker/fix-permissions.sh
@@ -12,6 +12,7 @@ d="/opt/testbed"
 chown -R $(stat -c '%u' "$d"):$(stat -c '%g' "$d") "$d"
 
 inotifywait -m -r "$d" -e create --format '%w%f' | while read f; do
+    if [[ "$f" == *".git"* ]]; then continue; fi
     chown $(stat -c '%u' "$d"):$(stat -c '%g' "$d") "$f"; \
     echo "$(date +%H:%M:%S.%N) Fixed $f"; \
 done
